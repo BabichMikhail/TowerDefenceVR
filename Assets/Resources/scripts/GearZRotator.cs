0ebab1678f1488c26ac2793c5faee6ca7bf703bb
Mikhail Babich <babich-mv@yandex.ru>
2018-03-31 00:44:48 +1000

    Increase gear rotation

--- Assets/Resources/scripts/GearZRotator.cs
+++ Assets/Resources/scripts/GearZRotator.cs
@@ -2,8 +2,13 @@
 
 public class GearZRotator : MonoBehaviour {
     public float speed = 1.0f;
+    public float randomRotationSpeed;
+
+    private void Start() {
+        randomRotationSpeed = Random.Range(1.0f, 1.5f);
+    }
 
     void Update () {
-        transform.Rotate(new Vector3(0, 0, 1.0f * Time.deltaTime * speed));
+        transform.Rotate(new Vector3(0, 0, 4.0f * randomRotationSpeed * Time.deltaTime * speed));
     }
 }
