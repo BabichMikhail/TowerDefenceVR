139d6f372457291db7dc25144f45a7459cfc39f6
Mikhail Babich <babich-mv@yandex.ru>
2018-05-05 00:55:10 +1000

    Remove mixed router, using agents and navigation

--- Assets/Resources/scripts/RouterController.cs
+++ Assets/Resources/scripts/RouterController.cs
@@ -5,9 +5,6 @@ public class RouterController : MonoBehaviour {
     public GameObject[] units;
     public int type;
 
-    public const int ROUTER_TYPE_MIXED = 0;
-    public const int ROUTER_TYPE_NAVMESH = 1;
-
     private BaseRouter router;
 
     public void SetRouter(BaseRouter router) { this.router = router; }
@@ -15,18 +12,11 @@ public class RouterController : MonoBehaviour {
 
     public BaseRouter GetEmptyRouter()
     {
-        BaseRouter router = null;
-        if (type == ROUTER_TYPE_MIXED)
-            router = new MixedRouter();
-        else if (type == ROUTER_TYPE_NAVMESH)
-            router = new NavMeshAgentRouter();
-        Debug.Assert(router != null);
-        return router;
+        return new NavMeshAgentRouter();
     }
 
     public void EnableUnit(GameObject unit)
     {
-        if (type == ROUTER_TYPE_NAVMESH)
-            unit.GetComponent<NavMeshAgent>().enabled = true;
+        unit.GetComponent<NavMeshAgent>().enabled = true;
     }
 }
