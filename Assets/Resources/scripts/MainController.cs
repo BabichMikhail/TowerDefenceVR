d8aab8328eb67de9f3816f854d994ef4b5ab0ba2
Mikhail Babich <babich-mv@yandex.ru>
2018-05-11 23:59:59 +1000

    Fix send units

--- Assets/Resources/scripts/MainController.cs
+++ Assets/Resources/scripts/MainController.cs
@@ -1,6 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+public class MyIntTuple
+{
+    public int first;
+    public int second;
+    public int third;
+
+    public MyIntTuple(int first, int second, int third) { this.first = first; this.second = second; this.third = third; }
+}
+
 public class MainController : MonoBehaviour {
     public GameObject mainTower;
     public GameObject[] towers0;
@@ -11,6 +20,25 @@ public class MainController : MonoBehaviour {
     private float lastIncreaseMoneyTime;
     private int unitWaveIndex = 0;
     private int lastSendUnitTime = -1000;
+
+    // time in milliseconds, unit count, respawn index
+    private List<MyIntTuple> UNIT_WAVES = new List<MyIntTuple>() {
+        new MyIntTuple(1, 1, 2),
+        new MyIntTuple(3000, 2, 0),
+        new MyIntTuple(23000, 1, 1),
+        new MyIntTuple(24000, 1, 2),
+        new MyIntTuple(30000, 1, 2),
+        new MyIntTuple(40000, 2, 0),
+        new MyIntTuple(43000, 3, 1),
+        new MyIntTuple(43000, 1, 2),
+        new MyIntTuple(80000, 5, 0),
+        new MyIntTuple(85000, 5, 1),
+        new MyIntTuple(120000, 8, 0),
+        new MyIntTuple(121000, 7, 1),
+        new MyIntTuple(122000, 4, 2),
+        new MyIntTuple(200000, 50, 0),
+        new MyIntTuple(200000, 50, 1),
+    };
 
     private void Awake()
     {
@@ -58,14 +86,13 @@ public class MainController : MonoBehaviour {
         CurrentTowerDefenceState.GetInstance().ChangeBalance(balanceDelta);
     }
 
-
     public void TryToSendUnit()
     {
         int now = (int)((Time.time - startedAt) * 1000);
-        if (lastSendUnitTime + Config.SEND_UNIT_INTERVAL < now && unitWaveIndex < Config.UNIT_WAVES.Count && Config.UNIT_WAVES[unitWaveIndex].first <= now) {
-            SendUnit(Config.UNIT_WAVES[unitWaveIndex].third);
-            --Config.UNIT_WAVES[unitWaveIndex].second;
-            if (Config.UNIT_WAVES[unitWaveIndex].second == 0)
+        if (lastSendUnitTime + Config.SEND_UNIT_INTERVAL < now && unitWaveIndex < UNIT_WAVES.Count && UNIT_WAVES[unitWaveIndex].first <= now) {
+            SendUnit(UNIT_WAVES[unitWaveIndex].third);
+            --UNIT_WAVES[unitWaveIndex].second;
+            if (UNIT_WAVES[unitWaveIndex].second == 0)
                 ++unitWaveIndex;
             lastSendUnitTime = now - Random.Range(0, Config.SEND_UNIT_INTERVAL / 3);
         }
